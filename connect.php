d21d559b317e17cc9903000c1c1f24bcdae4f77e
Mohammad Affan Asghar <113276865+affancoder@users.noreply.github.com>
2024-09-10 15:39:18 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+    $name = $_POST['name'];
+    $email =  $_POST['email'];
+    $contact= $_POST['contact'];
+    $whatsapp  = $_POST['whatsapp'];
+    $city= $_POST['city'];
+    $state = $_POST['state'];
+    $country = $_POST['country'];
+    $language = $_POST['language'];
+    $fluency = $_POST['fluency'];
+    $writing = $_POST['writing'];
+    $intro = $_POST['intro'];
+    $source = $_POST['source'];
+    $motivation = $_POST['motivation'];
+    $meeting_date = $_POST['meeting-date'];
+    $meeting_time = $_POST['meeting-time'];
+    $platform = $_POST['platform'];
+
+    
+
+?>
