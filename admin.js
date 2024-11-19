8dc40319281eb4034bca17ca9af3d5c6fda2380b
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2024-11-19 17:41:45 +0530

    Add files via upload

--- admin.js
+++ admin.js
@@ -3,3 +3,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.body.classList.add("loaded");
 });
+
+$(document).ready(function () {
+  $("#journalTable").DataTable({
+    paging: true,
+    searching: true,
+    lengthChange: true,
+    pageLength: 10,
+    info: true,
+    responsive: true,
+    columnDefs: [
+      { orderable: false, targets: 4 }, // Disables sorting for "Actions" column
+    ],
+  });
+});
