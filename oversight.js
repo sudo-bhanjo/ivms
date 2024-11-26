e939d8d883eaf485eb240207883a484dd575fbb6
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2024-11-26 13:29:50 +0530

    Add files via upload

---
+++ oversight.js
@@ -0,0 +1,17 @@
+// Show entries
+
+$(document).ready(function () {
+  $("table").DataTable({
+    pageLength: 10, // Default page length
+    lengthMenu: [
+      [10, 25, 50, 100],
+      [10, 25, 50, 100],
+    ], // Page length options
+  });
+});
+
+// ---- Smooth loading ----
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.body.classList.add("loaded");
+});
